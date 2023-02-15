1918fef86ee6985959019bd43674600c73f9af9f
micogongob <15572400+micogongob@users.noreply.github.com>
2023-02-15 15:41:41 +0800

    Rename util/helper class

--- libs/java-project-commons/src/main/java/com/template/javaproject/commons/util/JavaProjectHelper.java
+++ libs/java-project-commons/src/main/java/com/template/javaproject/commons/util/JavaProjectHelper.java
@@ -1,18 +1,15 @@
 package com.template.javaproject.commons.util;
 
-import lombok.NoArgsConstructor;
-import lombok.AccessLevel;
 import com.template.javaproject.commons.model.ApplicationInfo;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class JavaProjectUtil {
+public  class JavaProjectHelper {
     private static ApplicationInfo info = new ApplicationInfo(
         "java-project",
         "Java Project",
         "1.0.0"
     );
 
-    public static ApplicationInfo getAppInfo() {
+    public ApplicationInfo getAppInfo() {
         return info;
     }
 }
--- libs/java-project-commons/src/test/java/com/template/javaproject/commons/model/ApplicationInfoTest.java
+++ libs/java-project-commons/src/test/java/com/template/javaproject/commons/model/ApplicationInfoTest.java
@@ -8,12 +8,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ApplicationInfoTest {
     @Test
     void shouldMemberFields() {
+        // given
         ApplicationInfo info = new ApplicationInfo(
             UUID.randomUUID().toString(),
             UUID.randomUUID().toString(),
             UUID.randomUUID().toString()
         );
 
+        // expect
         assertThat(info.getName()).isNotBlank();
         assertThat(info.getDescription()).isNotBlank();
         assertThat(info.getVersion()).isNotBlank();
